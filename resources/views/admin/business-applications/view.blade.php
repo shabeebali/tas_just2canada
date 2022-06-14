4bb11f6215c35e5f50ba68568c77a048af075cf1
shabeeb <shabeeboali@gmail.com>
2022-06-14 19:35:18 +0530

    width of remark changed

--- resources/views/admin/business-applications/view.blade.php
+++ resources/views/admin/business-applications/view.blade.php
@@ -274,7 +274,7 @@
         </div>
     </x-blocks.card>
     <div id="popup-modal" tabindex="-1" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 md:inset-0 h-modal md:h-full">
-        <div class="relative p-4 w-full max-w-md h-full md:h-auto">
+        <div class="relative p-4 w-full max-w-4xl h-full md:h-auto">
             <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
                 <form action="{{ route('admin.form-remarks.store') }}" method="POST">
                     @csrf
