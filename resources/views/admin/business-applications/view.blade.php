3134b4cb351ab86eba80613bfe91d0061fc0c9f2
Shabeeb <shabeeboali@gmail.com>
2022-06-25 22:05:56 +0530

    minor bug in admin BA page

--- resources/views/admin/business-applications/view.blade.php
+++ resources/views/admin/business-applications/view.blade.php
@@ -331,7 +331,6 @@
                         <x-blocks.input-field type="file" class="mb-4" label="File 3" name="file_3"></x-blocks.input-field>
                         <x-blocks.input-field type="file" class="mb-4" label="File 4" name="file_4"></x-blocks.input-field>
                         <x-blocks.button type="submit" label="Save"></x-blocks.button>
-                        <button data-modal-toggle="popup-modal" type="button" class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">Cancel</button>
                     </x-blocks.card>
                 </form>
             </div>
