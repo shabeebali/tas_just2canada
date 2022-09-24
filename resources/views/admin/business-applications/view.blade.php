e66635c5d1522e3c3998f6030ef33343de0a87d5
shabe <shabeeboali@gmail.com>
2022-09-24 23:11:52 +0530

    final blow

--- resources/views/admin/business-applications/view.blade.php
+++ resources/views/admin/business-applications/view.blade.php
@@ -4,10 +4,12 @@
             <div class="font-bold pb-2.5 text-white bg-slate-700 flex justify-between px-4 py-2">
                 <div class="text-xl">Client ID: {{$data->client_id}}</div>
                 <div class="flex text-lime-300">
+                    @role('super_admin')
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m.75 12l3 3m0 0l3-3m-3 3v-6m-1.5-9H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z" />
                     </svg>
                     <a href="{{route('admin.business-applications.download',$data->id)}}">Download</a>
+                    @endrole
                 </div>
                 <div class="">Created
                     At: {{ $data->created_at->setTimezone('Canada/Eastern')->toDayDateTimeString() }}</div>
@@ -323,6 +325,8 @@
                             <strong> Intend to move to Canada
                                 in:</strong> {{ $data->form_data['intend_to_move'] ?? '' }}
                         </td>
+                    </tr>
+                    <tr class="{{ $rowClass }}">
                         <td class="{{ $valueClass }}">
                             <strong> Have you obtained an official assessment of your education by applying for an Education Credentials Assessment (ECA)?
                                 :</strong> {{ $data->form_data['have_you_obtained_educational_credentials_assessment'] ?? '' }}
--- resources/views/frontend/employer/register.blade.php
+++ resources/views/frontend/employer/register.blade.php
@@ -24,9 +24,28 @@
                             <div class="form-inner">
                                 <div class="col-md-12">Email id    <input id=" " type="text" class="form-control col-12" maxlength="40" name="email" value="{{old('email')}}"></div>
 
-                                <div class="col-md-12">Password:  <input id=" " type="password" class="form-control col-12" maxlength="40" name="password" value="{{old('password')}}"></div>
-                                <div class="col-md-12">Re-Enter Password:  <input id=" " type="password" class="form-control col-12" maxlength="40" name="password_confirmation"></div>
-
+                                <div class="col-md-12" x-data="{showText: false}">
+                                    Password: <input id=" " :type="showText ? 'text':'password'" class="form-control col-12" maxlength="40" name="password" value="{{old('password')}}">
+                                    <div class="d-flex moto-absolute-position items-center pr-3 cursor-pointer" style="top:6px;bottom:0;right: 22px; align-items: center" @click="showText = !showText">
+                                        <template x-if="showText">
+                                            <svg style="width: 1.75rem;height: 1.75rem;color: rgb(107 114 128);" class="w-6 h-6 text-gray-500 hover:text-gray-600 dark:text-gray-40" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"></path></svg>
+                                        </template>
+                                        <template x-if="!showText">
+                                            <svg style="width: 1.75rem;height: 1.75rem;color: rgb(107 114 128);" class="w-6 h-6 text-gray-500 hover:text-gray-600 dark:text-gray-40" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13.875 18.825A10.05 10.05 0 0112 19c-4.478 0-8.268-2.943-9.543-7a9.97 9.97 0 011.563-3.029m5.858.908a3 3 0 114.243 4.243M9.878 9.878l4.242 4.242M9.88 9.88l-3.29-3.29m7.532 7.532l3.29 3.29M3 3l3.59 3.59m0 0A9.953 9.953 0 0112 5c4.478 0 8.268 2.943 9.543 7a10.025 10.025 0 01-4.132 5.411m0 0L21 21"></path></svg>
+                                        </template>
+                                    </div>
+                                </div>
+                                <div class="col-md-12" x-data="{showText: false}">
+                                    Re-Enter Password: <input id=" " :type="showText ? 'text':'password'" class="form-control col-12" maxlength="40" name="password_confirmation" value="{{old('password')}}">
+                                    <div class="d-flex moto-absolute-position items-center pr-3 cursor-pointer" style="top:6px;bottom:0;right: 22px; align-items: center" @click="showText = !showText">
+                                        <template x-if="showText">
+                                            <svg style="width: 1.75rem;height: 1.75rem;color: rgb(107 114 128);" class="w-6 h-6 text-gray-500 hover:text-gray-600 dark:text-gray-40" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"></path></svg>
+                                        </template>
+                                        <template x-if="!showText">
+                                            <svg style="width: 1.75rem;height: 1.75rem;color: rgb(107 114 128);" class="w-6 h-6 text-gray-500 hover:text-gray-600 dark:text-gray-40" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13.875 18.825A10.05 10.05 0 0112 19c-4.478 0-8.268-2.943-9.543-7a9.97 9.97 0 011.563-3.029m5.858.908a3 3 0 114.243 4.243M9.878 9.878l4.242 4.242M9.88 9.88l-3.29-3.29m7.532 7.532l3.29 3.29M3 3l3.59 3.59m0 0A9.953 9.953 0 0112 5c4.478 0 8.268 2.943 9.543 7a10.025 10.025 0 01-4.132 5.411m0 0L21 21"></path></svg>
+                                        </template>
+                                    </div>
+                                </div>
                                 <center>
                                     <input type="submit" value="Submit" class="btn">
                                 </center>
