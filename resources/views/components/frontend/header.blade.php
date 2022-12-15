c6c41a813d990e655a133b7dc48260ae8e0241a8
shabe <shabeeboali@gmail.com>
2022-12-15 21:11:56 +0530

    fb pixel

--- resources/views/components/frontend/header.blade.php
+++ resources/views/components/frontend/header.blade.php
@@ -47,6 +47,23 @@
     <!--<script src="{{ asset('js/jquery-1.10.2.min.js') }}" ></script>  -->
     <link href="{{ asset('css/accordian.css') }}" rel="stylesheet" type="text/css" />
     <!--<script type="text/javascript" language="javascript" src="js/jquery.js"></script> -->
+    <!-- Meta Pixel Code -->
+    <script>
+        !function(f,b,e,v,n,t,s)
+        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '2204332279752101');
+        fbq('track', 'PageView');
+    </script>
+    <noscript>
+        <img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=2204332279752101&ev=PageView&noscript=1"/>
+    </noscript>
+    <!-- End Meta Pixel Code -->
 </head>
 <body id="home">
 
