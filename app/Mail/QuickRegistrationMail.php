d88a174f024482cc144a08fa66e05bec9210f0c7
shabeeb <shabeeboali@gmail.com>
2023-05-16 18:11:46 +0530

    quick registration form

--- app/Mail/QuickRegistrationMail.php
+++ app/Mail/QuickRegistrationMail.php
@@ -18,9 +18,9 @@ class QuickRegistrationMail extends Mailable
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(QuickRegistration $model)
     {
-        //
+        $this->model = $model;
     }
 
     /**
